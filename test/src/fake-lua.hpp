1f853277be026038a281ab2aff45cc63a04f9ae5
dor666 <dorian.apanel@gmail.com>
2014-04-01 00:08:11 +0200

    Working test (not passing) of creating empty module with certain name.

--- test/src/fake-lua.hpp
+++ test/src/fake-lua.hpp
@@ -8,7 +8,8 @@ public:
 
     MOCK_METHOD1(lua_upvalueindex, int(int i));
     MOCK_METHOD2(lua_topointer, const void*(Lua_state_mock* l, int index));
-
+    MOCK_METHOD1(lua_newtable, void(Lua_state_mock *L));
+    MOCK_METHOD2(lua_setglobal, void(Lua_state_mock* L, const char* name));
 };
 
 typedef Lua_state_mock lua_State;
@@ -25,4 +26,16 @@ inline
 const void* lua_topointer (lua_State *L, int index)
 {
     return The_lua_state_mock->lua_topointer(L, index);
+}
+
+inline
+void lua_newtable (lua_State *L)
+{
+    return The_lua_state_mock->lua_newtable(L);
+}
+
+inline
+void lua_setglobal (lua_State *L, const char *name)
+{
+    return The_lua_state_mock->lua_setglobal(L, name);
 }
--- test/src/module-test.cpp
+++ test/src/module-test.cpp
@@ -4,11 +4,14 @@
 
 using ::testing::_;
 
-TEST(Module_test, Register_module)
+TEST(Module_test, Register_global_module)
 {
     The_lua_state_mock = new Lua_state_mock();
-    //EXPECT_CALL(*The_lua_state_mock, lua_upvalueindex(_));
+    Lua_state_mock* lsm = The_lua_state_mock; //shortcut
+    const char* module_name = "FooModule";
 
+    EXPECT_CALL(*lsm, lua_newtable(lsm));
+    EXPECT_CALL(*lsm, lua_setglobal(lsm, module_name));
 
 
     delete The_lua_state_mock;
